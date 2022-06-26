2b1ca6a83a4469cbd6370a7acd6d3e9f7ec660f4
WebShopSriLanka <srilankawebshop@gmail.com>
2022-06-26 13:54:55 +0530

    Tech stack UI preliminary draft added

--- modules/first-screen-module.php
+++ modules/first-screen-module.php
@@ -1,4 +1,6 @@
 
 <?php 
-    include 'views/ui/first-screen.php';
+    // include 'views/ui/first-screen.php';
+    // include 'views/ui/doorway.php';
+    include 'views/ui/welcome-landscape.php';
 ?>
--- views/ui/welcome-landscape.php
+++ views/ui/welcome-landscape.php
@@ -1,30 +1,102 @@
 <?php
 // Welcome UI with buttons
 ?>
-<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+<!-- <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script> -->
 
 <div class="outer">
     <div class="welcome-landscape landscape flex flex-center cf">
 
         <div class="col design-col flex flex-center cf">
-            <div class="unit line-man">
-                <img src="media/img/artwork/line-man-code.png" alt="">
-            </div>
 
-            <div class="unit unit20 gif">
-                <div class="">
-                    <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_vnikrcia.json" background="transparent" speed="1" style="width: 360px; height: 360px;" loop autoplay></lottie-player>
+            <div class="unit-group">
+                <div class="unit line-man">
+                    <img src="media/img/artwork/line-man-thinking.png" alt="">
                 </div>
             </div>
+            <!-- unit-group -->
 
 
-            <div class="unit logo logo-wide photoshop-logo slide-bottom">
-                <img src="media/img/logos/photoshop-logo-white.png" alt="Photoshop">
+            <div class="unit-group">
+                <div class="unit unit20 gif">
+                    <div class="">
+                        <!-- <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_vnikrcia.json" background="transparent" speed="1" style="width: 360px; height: 360px;" autoplay></lottie-player> -->
+                    </div>
+                </div>
             </div>
+            <!-- unit-group -->
+
+
+
+            <div class="unit-group">
+                <div class="unit logo logo-wide data-mining-logo slide-bottom">
+                    <img src="media/img/logos/data-mining-orange-voyant-logo.png" alt="Photoshop">
+                </div>
+            </div>
+            <!-- unit-group -->
+
+
+
+            <div class="unit-group">
+                <div class="unit logo logo-extra-wide google-ga-logo slide-bottom">
+                    <img src="media/img/logos/google-analyytics-console-logo.png" alt="">
+                </div>
+            </div>
+            <!-- unit-group -->
+
         </div>
         <!-- design-col -->
 
+
+
+
+
+
         <div class="col code-col cf">
+
+            <div class="unit-group">
+                <div class="unit line-man">
+                    <img src="media/img/artwork/line-man-coding.png" alt="">
+                </div>
+            </div>
+            <!-- unit-group -->
+
+
+            <div class="unit-group">
+
+
+            <div class="unit logo vscode-logo slide-bottom">
+                    <img src="media/img/logos/vscode-logo.png" alt="">
+                </div>
+
+                <div class="unit logo html-logo slide-bottom">
+                    <img src="media/img/logos/html-css-logo.png" alt="">
+                </div>
+
+                <div class="unit logo js-logo slide-bottom-2">
+                    <img src="media/img/logos/js-jquery-logo.png" alt="">
+                </div>
+
+                <div class="unit logo php-logo slide-bottom">
+                    <img src="media/img/logos/php8-logo.png" alt="">
+                </div>
+
+                <div class="unit logo sql-logo slide-bottom-2">
+                    <img src="media/img/logos/sql-mysql-logo.png" alt="">
+                </div>
+                
+                <div class="unit logo react-logo slide-bottom">
+                    <img src="media/img/logos/react-logo.png" alt="">
+                </div>
+                
+                <div class="unit logo nodejs-logo slide-bottom-2">
+                    <img src="media/img/logos/nodejs-logo.png" alt="">
+                </div>
+            </div>
+            <!-- unit-group -->
+
+
+
+
 
         </div>
         <!-- code-col -->
